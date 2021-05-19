5a8a121c6d92c1490752fb6b573540fffa95f608
TshepoPhala <82643675+TshepoPhala@users.noreply.github.com>
2021-05-19 17:01:19 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+function myFunc(userid)
+{
+var text = document.getElementById(userid).value;
+
+document.getElementById("posts").innerHTML = text;
+//document.getElementById("posts").innerHTML = text;
+}
